e67056c96458120f0b0259b29da1cf73a64efcfc
eunbyul616 <eunbyul616@gmail.com>
2022-11-14 00:38:28 +0900

    feat&build: date 리턴 형태 수정 및 requirements.txt 추가

--- date/date.py
+++ date/date.py
@@ -3,12 +3,12 @@ from datetime import datetime, timedelta
 class Date:
     @classmethod
     def today(cls):
-        return datetime.now()
+        return datetime.now().strftime('%Y-%m-%d')
 
     @classmethod
     def yesterday(cls):
-        return datetime.now() - timedelta(days=1)
+        return (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
 
     @classmethod
     def tomorrow(cls):
-        return datetime.now() + timedelta(days=1)
+        return (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d')

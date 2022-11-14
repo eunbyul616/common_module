dd1b080ace0abc0d4f47219c8e388d55a1db94ca
eunbyul616 <eunbyul616@gmail.com>
2022-11-14 21:19:54 +0900

    feat: poetry 관련 파일 수정 및 .gitignore 파일 수정

--- make_project.sh
+++ make_project.sh
@@ -3,9 +3,25 @@
 MODULE_PATH="$( cd "$( dirname "$0" )" && pwd -P )"
 PROJECT_PATH=${MODULE_PATH%/*}
 
+# copy template 
 cp -r $MODULE_PATH/template/* $PROJECT_PATH
 
-pip3 install virtualenv
-python3 -m virtualenv venv
-source venv/bin/activate
-pip3 install -r $MODULE_PATH/requirements.txt
+# copy pyproject.toml file
+cp $MODULE_PATH/pyproject.toml $PROJECT_PATH/
+
+# copy .gitignore
+cp MODULE_PATH/gitignore $PROJECT_PATH/
+
+# setting environment
+#pip3 install virtualenv
+#python3 -m virtualenv venv
+#source venv/bin/activate
+
+# install packages
+#pip3 install -r $MODULE_PATH/requirements.txt
+
+# poetry
+poetry init
+poetry config virtualenvs.in-project true
+poetry shell
+poetry install

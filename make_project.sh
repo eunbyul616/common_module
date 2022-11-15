f0575775eb1e022ce0750fc07281d13b49dd0664
eunbyul616 <eunbyul616@gmail.com>
2022-11-15 22:14:45 +0900

    feat: pipenv 관련 추가

--- make_project.sh
+++ make_project.sh
@@ -4,27 +4,28 @@ MODULE_PATH="$( cd "$( dirname "$0" )" && pwd -P )"
 PROJECT_PATH=${MODULE_PATH%/*}
 
 # copy template
-cp -r $MODULE_PATH/template/* $PROJECT_PATH
+#cp -r $MODULE_PATH/template/* $PROJECT_PATH
 
 # copy pyproject.toml file
 #cp $MODULE_PATH/pyproject.toml $PROJECT_PATH/
 
 # copy .gitignore
-cp $MODULE_PATH/.gitignore $PROJECT_PATH/
+#cp $MODULE_PATH/.gitignore $PROJECT_PATH/
 
 # setting environment
 # install pyenv, pyenv-virtualenv
 # brew install pyenv
 # brew install pyenv-virtualenv
-pyenv install 3.9.6
-pyenv virtualenv venv
-pyenv activate venv
+# brew install pipenv
+pipenv --python 3.9.6
+pipenv shell
 
 # linux, mac os
-source venv/bin/activate
+#source venv/bin/activate
 
 # install packages
-pip install -r $MODULE_PATH/requirements.txt
+pipenv install
+#pip install -r $MODULE_PATH/requirements.txt
 
 # poetry
 #cd $PROJECT_PATH

d033a6e32eebb744d786e8d9cb5923e37bcc81e8
이은별(AI 개발) - 데이터인텔리전스팀 <eunbyul@shinsegae.com>
2022-11-15 22:29:27 +0900

    feat: make_project.sh 파일 수정

--- make_project.sh
+++ make_project.sh
@@ -4,19 +4,19 @@ MODULE_PATH="$( cd "$( dirname "$0" )" && pwd -P )"
 PROJECT_PATH=${MODULE_PATH%/*}
 
 # copy template
-#cp -r $MODULE_PATH/template/* $PROJECT_PATH
+cp -r $MODULE_PATH/template/* $PROJECT_PATH
 
 # copy pyproject.toml file
 #cp $MODULE_PATH/pyproject.toml $PROJECT_PATH/
 
 # copy .gitignore
-#cp $MODULE_PATH/.gitignore $PROJECT_PATH/
+cp $MODULE_PATH/.gitignore $PROJECT_PATH/
 
 # setting environment
 # install pyenv, pyenv-virtualenv
 # brew install pyenv
 # brew install pyenv-virtualenv
-# brew install pipenv
+brew install pipenv
 pipenv --python 3.9.6
 pipenv shell
 
